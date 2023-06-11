19db48047bd2ca6915c22b713e7363f6e679eab8
haitham314s <haitham314s@gmail.com>
2023-06-11 18:06:56 +0400

    reset textarea input after submit + made tweet a required field

--- tweets/models.py
+++ tweets/models.py
@@ -7,5 +7,8 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
+    class Meta:
+        ordering = ["-id"]
+
     def serialize(self):
         return {"id": self.id, "content": self.content, "likes": randint(0, 999)}
