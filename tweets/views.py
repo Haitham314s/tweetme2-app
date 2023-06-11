16ec2615201d7278103cf840aabfa7a617ee9049
haitham314s <haitham314s@gmail.com>
2023-06-11 17:49:25 +0400

    if create is xhr, returns json response instead of html response

--- tweets/views.py
+++ tweets/views.py
@@ -16,12 +16,16 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    is_ajax = request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
 
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+
+        if is_ajax:
+            return JsonResponse({}, status=201)
         if next_url and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
 
